fbe22aedb0bb95f6597bd26ce4fe95ea0ecc54ee
Thomas Pons <thomas.pons@microej.com>
2025-08-21 11:46:53 +0200

    Update README, CHANGELOG

--- src/main/java/com/mycompany/mymock/MyClassNatives.java
+++ src/main/java/com/mycompany/mymock/MyClassNatives.java
@@ -2,6 +2,9 @@ package com.mycompany.mymock;
 
 public class MyClassNatives {
 
+    /**
+     * Mock implementation of {@code public static native void printHello()} from application code.
+     */
     public static void printHello() {
         System.out.println("Hello");
     }
